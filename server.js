a156e1761a3b25e6419c7994f39abfb5566608e4
pixrael <pixrael777@gmail.com>
2021-04-08 12:03:00 -0300

    add middleware to server a favicon

--- server.js
+++ server.js
@@ -1,6 +1,9 @@
 const express = require('express');
 const app = express();
+const favicon = require('serve-favicon');
+const path = require('path');
 
+app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 
 app.get('/', (req, res) => {
     res.json({ products: [] });
