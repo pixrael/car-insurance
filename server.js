701572c05a9b8893db394ed3588df8671ee386f9
pixrael <pixrael777@gmail.com>
2021-04-08 11:54:21 -0300

    create basic server that returns an empty products array

--- server.js
+++ server.js
@@ -1,1 +1,11 @@
-console.log('hello world!');
+const express = require('express');
+const app = express();
+
+
+app.get('/', (req, res) => {
+    res.json({ products: [] });
+});
+
+const PORT = 3000 || process.env.PORT;
+
+app.listen(PORT, () => console.log(`server running on port ${PORT}`));
