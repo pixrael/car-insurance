a9fca448307ae71661f5f94afbc4c32c61c27773
pixrael <pixrael777@gmail.com>
2021-04-08 14:40:42 -0300

    add test to check decrease of the days and half the price when product expires

--- test/car-insurance-test.js
+++ test/car-insurance-test.js
@@ -1,4 +1,5 @@
 const assert = require('assert');
+const { Product } = require('../utils/product');
 const { CarInsurance } = require('../utils/car-insurance');
 
 
@@ -13,5 +14,50 @@ describe('Product Insurance |', function () {
             assert.ok((updatedProducts.constructor.name == "Array"));
             assert.ok((updatedProducts.length === 0));
         });
+
+
+    });
+});
+
+describe('Product Insurance | ', function () {
+    describe('CarInsurance.updatePrice - ', function () {
+
+        it('should reduce the number of days (sellIn) every time the update function is called', function () {
+
+            const carInsurance = new CarInsurance([new Product('Medium Coverage', 10, 20)]);
+            const updatedProducts = carInsurance.updatePrice();
+
+
+            assert.strictEqual(updatedProducts[0].sellIn, 9);
+
+        });
+
+        
+
+        it('should reduce to the half of the price when the product expires, sellIn === 0', function () {
+
+            const carInsurance = new CarInsurance([new Product('Medium Coverage', 10, 20)]);
+
+            let updatedProducts;
+            let i = 0;
+            let days = 0;
+            const totalDays = 9;
+            while (i < totalDays) {
+                updatedProducts = carInsurance.updatePrice();
+                i++;
+                days++;
+                assert.strictEqual(updatedProducts[0].sellIn, 10 - days);
+                assert.strictEqual(updatedProducts[0].price, 20);
+            }
+
+            updatedProducts = carInsurance.updatePrice();
+
+
+            assert.strictEqual(updatedProducts[0].sellIn, 0);
+            assert.strictEqual(updatedProducts[0].price, 10);
+
+        });
+
+
     });
 });
--- utils/car-insurance.js
+++ utils/car-insurance.js
@@ -3,7 +3,16 @@ class CarInsurance {
         this.products = products;
     }
     updatePrice() {
-        
+
+        this.products.filter(product => product.sellIn > 0).forEach(product => {
+            product.sellIn--;
+
+            if (product.sellIn === 0) { // just expired
+                product.price = product.price / 2;
+            }
+
+        });
+
         return this.products;
     }
 }
